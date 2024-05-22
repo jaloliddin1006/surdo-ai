e76a4bbdeda68dff1f8fd1b6158c2ccc58caed77
Jaloliddin <jmamatmusayev@gmail.com>
2024-05-22 14:16:49 +0500

    some update

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# ict-surdo
-real time surdo translator
+# Surdo AI
+
+### Real Time Surdo translator
+
+> Ushbu loyiha eshitish va muloqotda muammolari bo‘lgan insonlarga jamiyatda o‘z o‘rnini topishga va davlat xizmatlaridan to‘liq foydalanishga yordam beradi.
+
+>
+![alt text](static/image.png)
+
+-
+
--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,7 @@ from home import views
 
 urlpatterns = [
     path('', views.HomaPageView.as_view(), name='home'),
-    path('camera/', views.CameraPageView.as_view(), name='camera'),
-    path('voice/', views.VoicePageView.as_view(), name='voice'),
+    path('voice-surdo/', views.CameraPageView.as_view(), name='camera'),
+    path('surdo-voice/', views.VoicePageView.as_view(), name='voice'),
     path('video/', views.VideoPageView.as_view(), name='video'),
 ]
