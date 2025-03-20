9f6ca1a49129bf3856d7356fe96af63eba31b778
Jaloliddin Mamatmusayev <jmamatmusayev@gmail.com>
2025-03-20 11:10:59 +0500

    Update main.js

--- static/main.js
+++ static/main.js
@@ -51,10 +51,6 @@ document.addEventListener("DOMContentLoaded", function() { // On DOM Load initia
 
 
 
-
-
-
-
 //////////////////////////////////////////////////////////
 window.addEventListener("load", () => {
     const bar = document.querySelectorAll(".bar");
